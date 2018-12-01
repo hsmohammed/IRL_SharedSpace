9b8433ca5f606c50d5845c7d8df619a0f61b2a8f
hsmohammed <hos40044@mail.ubc.ca>
2018-11-30 17:49:34 -0800

    Trajectory plotting

--- DataPlotting.R
+++ DataPlotting.R
@@ -1,215 +1,148 @@
+source("loadLibraries.R")
+
+
 Traj_Obj_111x_clipped2 %>% 
   ggplot(aes(speed_SGF))+geom_histogram(col = "black", fill = "lightblue3")
 
-ima_212<- readJPEG("Brk-2-1-world.png")
+ima_212<- readJPEG("Map/RB-1-1-world.jpg")
 
 arrow1 <- arrow(angle = 30, length = unit(0.05, "inches"),ends = "last", type = "open")
 
-for (i in 1:length(objects_2121_dir2$V1)) {
-  filename1 <- paste("plots/TrjMap/212x_dir2/2121",objects_2121_dir2$V1[i],sep = "_")
+for (i in 1:length(objects_1111$Obj)) {
+  filename1 <- paste("plots/world_trajectory_overlay/1111",objects_1111$Obj[i],sep = "_")
   fullname <- paste(filename1, ".jpg",sep = "")
-  p1 <- Traj_Obj_212x_clipped2_dir2[(Traj_Obj_212x_clipped2_dir2$comp == "2121"&Traj_Obj_212x_clipped2_dir2$Obj == objects_2121_dir2$V1[i]),] %>% 
-    ggplot(aes(X,Y, col = V_mps_corrected, group = Obj))+
+  p1 <- Traj_Obj_111x_clipped2[(Traj_Obj_111x_clipped2$comp == "1111"&Traj_Obj_111x_clipped2$Obj == objects_1111$Obj[i]),] %>% 
+    ggplot(aes(X_SGF,Y_SGF, col = type, group = Obj))+
     annotation_custom(rasterGrob(ima_212, width = unit(1,"npc"), 
                                  height = unit(1,"npc")), 
                       0, 108, 0, 74)+
-    geom_path(na.rm = TRUE, arrow = arrow1, size = 1) +  scale_color_distiller(palette = "RdYlGn",direction = 1)+
+    geom_path(na.rm = TRUE, arrow = arrow1, size = 1) +
     scale_x_continuous(limits = c(0,108), expand = c(0,0))+
     scale_y_continuous(limits = c(0,93), expand = c(0,0))
   ggsave(filename = fullname,p1)
 }
 
-for (i in 1:length(objects_2122_dir2$V1)) {
-  filename1 <- paste("plots/TrjMap/212x_dir2/2122",objects_2122_dir2$V1[i],sep = "_")
+for (i in 1:length(objects_1112$Obj)) {
+  filename1 <- paste("plots/world_trajectory_overlay/1112",objects_1112$Obj[i],sep = "_")
   fullname <- paste(filename1, ".jpg",sep = "")
-  p1 <- Traj_Obj_212x_clipped2_dir2[(Traj_Obj_212x_clipped2_dir2$comp == "2122"&Traj_Obj_212x_clipped2_dir2$Obj == objects_2122_dir2$V1[i]),] %>% 
-    ggplot(aes(X,Y, col = V_mps_corrected, group = Obj))+
+  p1 <- Traj_Obj_111x_clipped2[(Traj_Obj_111x_clipped2$comp == "1112"&Traj_Obj_111x_clipped2$Obj == objects_1112$Obj[i]),] %>% 
+    ggplot(aes(X_SGF,Y_SGF, col = type, group = Obj))+
     annotation_custom(rasterGrob(ima_212, width = unit(1,"npc"), 
                                  height = unit(1,"npc")), 
                       0, 108, 0, 74)+
-    geom_path(na.rm = TRUE, arrow = arrow1, size = 1) +  scale_color_distiller(palette = "RdYlGn",direction = 1)+
+    geom_path(na.rm = TRUE, arrow = arrow1, size = 1) +
     scale_x_continuous(limits = c(0,108), expand = c(0,0))+
     scale_y_continuous(limits = c(0,93), expand = c(0,0))
   ggsave(filename = fullname,p1)
 }
 
-for (i in 1:length(objects_2123_dir2$V1)) {
-  filename1 <- paste("plots/TrjMap/212x_dir2/2123",objects_2123_dir2$V1[i],sep = "_")
+for (i in 1:length(objects_1113$Obj)) {
+  filename1 <- paste("plots/world_trajectory_overlay/1113",objects_1113$Obj[i],sep = "_")
   fullname <- paste(filename1, ".jpg",sep = "")
-  p1 <- Traj_Obj_212x_clipped2_dir2[(Traj_Obj_212x_clipped2_dir2$comp == "2123"&Traj_Obj_212x_clipped2_dir2$Obj == objects_2123_dir2$V1[i]),] %>% 
-    ggplot(aes(X,Y, col = V_mps_corrected, group = Obj))+
+  p1 <- Traj_Obj_111x_clipped2[(Traj_Obj_111x_clipped2$comp == "1113"&Traj_Obj_111x_clipped2$Obj == objects_1113$Obj[i]),] %>% 
+    ggplot(aes(X_SGF,Y_SGF, col = type, group = Obj))+
     annotation_custom(rasterGrob(ima_212, width = unit(1,"npc"), 
                                  height = unit(1,"npc")), 
                       0, 108, 0, 74)+
-    geom_path(na.rm = TRUE, arrow = arrow1, size = 1) +  scale_color_distiller(palette = "RdYlGn",direction = 1)+
+    geom_path(na.rm = TRUE, arrow = arrow1, size = 1) +
     scale_x_continuous(limits = c(0,108), expand = c(0,0))+
     scale_y_continuous(limits = c(0,93), expand = c(0,0))
   ggsave(filename = fullname,p1)
 }
 
-for (i in 1:length(objects_2124_dir2$V1)) {
-  filename1 <- paste("plots/TrjMap/212x_dir2/2124",objects_2124_dir2$V1[i],sep = "_")
+for (i in 1:length(objects_1114$Obj)) {
+  filename1 <- paste("plots/world_trajectory_overlay/1114",objects_1114$Obj[i],sep = "_")
   fullname <- paste(filename1, ".jpg",sep = "")
-  p1 <- Traj_Obj_212x_clipped2_dir2[(Traj_Obj_212x_clipped2_dir2$comp == "2124"&Traj_Obj_212x_clipped2_dir2$Obj == objects_2124_dir2$V1[i]),] %>% 
-    ggplot(aes(X,Y, col = V_mps_corrected, group = Obj))+
+  p1 <- Traj_Obj_111x_clipped2[(Traj_Obj_111x_clipped2$comp == "1114"&Traj_Obj_111x_clipped2$Obj == objects_1114$Obj[i]),] %>% 
+    ggplot(aes(X_SGF,Y_SGF, col = type, group = Obj))+
     annotation_custom(rasterGrob(ima_212, width = unit(1,"npc"), 
                                  height = unit(1,"npc")), 
                       0, 108, 0, 74)+
-    geom_path(na.rm = TRUE, arrow = arrow1, size = 1) +  scale_color_distiller(palette = "RdYlGn",direction = 1)+
+    geom_path(na.rm = TRUE, arrow = arrow1, size = 1) +
     scale_x_continuous(limits = c(0,108), expand = c(0,0))+
     scale_y_continuous(limits = c(0,93), expand = c(0,0))
   ggsave(filename = fullname,p1)
 }
 
-for (i in 1:length(objects_2125_dir2$V1)) {
-  filename1 <- paste("plots/TrjMap/212x_dir2/2125",objects_2125_dir2$V1[i],sep = "_")
+for (i in 1:length(objects_1115$Obj)) {
+  filename1 <- paste("plots/world_trajectory_overlay/1115",objects_1115$Obj[i],sep = "_")
   fullname <- paste(filename1, ".jpg",sep = "")
-  p1 <- Traj_Obj_212x_clipped2_dir2[(Traj_Obj_212x_clipped2_dir2$comp == "2125"&Traj_Obj_212x_clipped2_dir2$Obj == objects_2125_dir2$V1[i]),] %>% 
-    ggplot(aes(X,Y, col = V_mps_corrected, group = Obj))+
+  p1 <- Traj_Obj_111x_clipped2[(Traj_Obj_111x_clipped2$comp == "1115"&Traj_Obj_111x_clipped2$Obj == objects_1115$Obj[i]),] %>% 
+    ggplot(aes(X_SGF,Y_SGF, col = type, group = Obj))+
     annotation_custom(rasterGrob(ima_212, width = unit(1,"npc"), 
                                  height = unit(1,"npc")), 
                       0, 108, 0, 74)+
-    geom_path(na.rm = TRUE, arrow = arrow1, size = 1) +  scale_color_distiller(palette = "RdYlGn",direction = 1)+
+    geom_path(na.rm = TRUE, arrow = arrow1, size = 1) +
     scale_x_continuous(limits = c(0,108), expand = c(0,0))+
     scale_y_continuous(limits = c(0,93), expand = c(0,0))
   ggsave(filename = fullname,p1)
 }
 
-for (i in 1:length(objects_2126_dir2$V1)) {
-  filename1 <- paste("plots/TrjMap/212x_dir2/2126",objects_2126_dir2$V1[i],sep = "_")
+for (i in 1:length(objects_1116$Obj)) {
+  filename1 <- paste("plots/world_trajectory_overlay/1116",objects_1116$Obj[i],sep = "_")
   fullname <- paste(filename1, ".jpg",sep = "")
-  p1 <- Traj_Obj_212x_clipped2_dir2[(Traj_Obj_212x_clipped2_dir2$comp == "2126"&Traj_Obj_212x_clipped2_dir2$Obj == objects_2126_dir2$V1[i]),] %>% 
-    ggplot(aes(X,Y, col = V_mps_corrected, group = Obj))+
+  p1 <- Traj_Obj_111x_clipped2[(Traj_Obj_111x_clipped2$comp == "1116"&Traj_Obj_111x_clipped2$Obj == objects_1116$Obj[i]),] %>% 
+    ggplot(aes(X_SGF,Y_SGF, col = type, group = Obj))+
     annotation_custom(rasterGrob(ima_212, width = unit(1,"npc"), 
                                  height = unit(1,"npc")), 
                       0, 108, 0, 74)+
-    geom_path(na.rm = TRUE, arrow = arrow1, size = 1) +  scale_color_distiller(palette = "RdYlGn",direction = 1)+
+    geom_path(na.rm = TRUE, arrow = arrow1, size = 1) +
     scale_x_continuous(limits = c(0,108), expand = c(0,0))+
     scale_y_continuous(limits = c(0,93), expand = c(0,0))
   ggsave(filename = fullname,p1)
 }
 
-for (i in 1:length(objects_2127_dir2$V1)) {
-  filename1 <- paste("plots/TrjMap/212x_dir2/2127",objects_2127_dir2$V1[i],sep = "_")
+for (i in 1:length(objects_1117$Obj)) {
+  filename1 <- paste("plots/world_trajectory_overlay/1117",objects_1117$Obj[i],sep = "_")
   fullname <- paste(filename1, ".jpg",sep = "")
-  p1 <- Traj_Obj_212x_clipped2_dir2[(Traj_Obj_212x_clipped2_dir2$comp == "2127"&Traj_Obj_212x_clipped2_dir2$Obj == objects_2127_dir2$V1[i]),] %>% 
-    ggplot(aes(X,Y, col = V_mps_corrected, group = Obj))+
+  p1 <- Traj_Obj_111x_clipped2[(Traj_Obj_111x_clipped2$comp == "1117"&Traj_Obj_111x_clipped2$Obj == objects_1117$Obj[i]),] %>% 
+    ggplot(aes(X_SGF,Y_SGF, col = type, group = Obj))+
     annotation_custom(rasterGrob(ima_212, width = unit(1,"npc"), 
                                  height = unit(1,"npc")), 
                       0, 108, 0, 74)+
-    geom_path(na.rm = TRUE, arrow = arrow1, size = 1) +  scale_color_distiller(palette = "RdYlGn",direction = 1)+
+    geom_path(na.rm = TRUE, arrow = arrow1, size = 1) +
     scale_x_continuous(limits = c(0,108), expand = c(0,0))+
     scale_y_continuous(limits = c(0,93), expand = c(0,0))
   ggsave(filename = fullname,p1)
 }
 
-for (i in 1:length(objects_2128_dir2$V1)) {
-  filename1 <- paste("plots/TrjMap/212x_dir2/2128",objects_2128_dir2$V1[i],sep = "_")
+for (i in 1:length(objects_1118$Obj)) {
+  filename1 <- paste("plots/world_trajectory_overlay/1118",objects_1118$Obj[i],sep = "_")
   fullname <- paste(filename1, ".jpg",sep = "")
-  p1 <- Traj_Obj_212x_clipped2_dir2[(Traj_Obj_212x_clipped2_dir2$comp == "2128"&Traj_Obj_212x_clipped2_dir2$Obj == objects_2128_dir2$V1[i]),] %>% 
-    ggplot(aes(X,Y, col = V_mps_corrected, group = Obj))+
+  p1 <- Traj_Obj_111x_clipped2[(Traj_Obj_111x_clipped2$comp == "1118"&Traj_Obj_111x_clipped2$Obj == objects_1118$Obj[i]),] %>% 
+    ggplot(aes(X_SGF,Y_SGF, col = type, group = Obj))+
     annotation_custom(rasterGrob(ima_212, width = unit(1,"npc"), 
                                  height = unit(1,"npc")), 
                       0, 108, 0, 74)+
-    geom_path(na.rm = TRUE, arrow = arrow1, size = 1) +  scale_color_distiller(palette = "RdYlGn",direction = 1)+
+    geom_path(na.rm = TRUE, arrow = arrow1, size = 1) +
     scale_x_continuous(limits = c(0,108), expand = c(0,0))+
     scale_y_continuous(limits = c(0,93), expand = c(0,0))
   ggsave(filename = fullname,p1)
 }
 
-for (i in 1:length(objects_2129_dir2$V1)) {
-  filename1 <- paste("plots/TrjMap/212x_dir2/2129",objects_2129_dir2$V1[i],sep = "_")
+for (i in 1:length(objects_1119$Obj)) {
+  filename1 <- paste("plots/world_trajectory_overlay/1119",objects_1119$Obj[i],sep = "_")
   fullname <- paste(filename1, ".jpg",sep = "")
-  p1 <- Traj_Obj_212x_clipped2_dir2[(Traj_Obj_212x_clipped2_dir2$comp == "2129"&Traj_Obj_212x_clipped2_dir2$Obj == objects_2129_dir2$V1[i]),] %>% 
-    ggplot(aes(X,Y, col = V_mps_corrected, group = Obj))+
+  p1 <- Traj_Obj_111x_clipped2[(Traj_Obj_111x_clipped2$comp == "1119"&Traj_Obj_111x_clipped2$Obj == objects_1119$Obj[i]),] %>% 
+    ggplot(aes(X_SGF,Y_SGF, col = type, group = Obj))+
     annotation_custom(rasterGrob(ima_212, width = unit(1,"npc"), 
                                  height = unit(1,"npc")), 
                       0, 108, 0, 74)+
-    geom_path(na.rm = TRUE, arrow = arrow1, size = 1) +  scale_color_distiller(palette = "RdYlGn",direction = 1)+
+    geom_path(na.rm = TRUE, arrow = arrow1, size = 1) +
     scale_x_continuous(limits = c(0,108), expand = c(0,0))+
     scale_y_continuous(limits = c(0,93), expand = c(0,0))
   ggsave(filename = fullname,p1)
 }
 
-for (i in 1:length(objects_21210_dir2$V1)) {
-  filename1 <- paste("plots/TrjMap/212x_dir2/21210",objects_21210_dir2$V1[i],sep = "_")
+for (i in 1:length(objects_11110$Obj)) {
+  filename1 <- paste("plots/world_trajectory_overlay/11110",objects_11110$Obj[i],sep = "_")
   fullname <- paste(filename1, ".jpg",sep = "")
-  p1 <- Traj_Obj_212x_clipped2_dir2[(Traj_Obj_212x_clipped2_dir2$comp == "21210"&Traj_Obj_212x_clipped2_dir2$Obj == objects_21210_dir2$V1[i]),] %>% 
-    ggplot(aes(X,Y, col = V_mps_corrected, group = Obj))+
+  p1 <- Traj_Obj_111x_clipped2[(Traj_Obj_111x_clipped2$comp == "11110"&Traj_Obj_111x_clipped2$Obj == objects_11110$Obj[i]),] %>% 
+    ggplot(aes(X_SGF,Y_SGF, col = type, group = Obj))+
     annotation_custom(rasterGrob(ima_212, width = unit(1,"npc"), 
                                  height = unit(1,"npc")), 
                       0, 108, 0, 74)+
-    geom_path(na.rm = TRUE, arrow = arrow1, size = 1) +  scale_color_distiller(palette = "RdYlGn",direction = 1)+
-    scale_x_continuous(limits = c(0,108), expand = c(0,0))+
-    scale_y_continuous(limits = c(0,93), expand = c(0,0))
-  ggsave(filename = fullname,p1)
-}
-
-for (i in 1:length(objects_21211_dir2$V1)) {
-  filename1 <- paste("plots/TrjMap/212x_dir2/21211",objects_21211_dir2$V1[i],sep = "_")
-  fullname <- paste(filename1, ".jpg",sep = "")
-  p1 <- Traj_Obj_212x_clipped2_dir2[(Traj_Obj_212x_clipped2_dir2$comp == "21211"&Traj_Obj_212x_clipped2_dir2$Obj == objects_21211_dir2$V1[i]),] %>% 
-    ggplot(aes(X,Y, col = V_mps_corrected, group = Obj))+
-    annotation_custom(rasterGrob(ima_212, width = unit(1,"npc"), 
-                                 height = unit(1,"npc")), 
-                      0, 108, 0, 74)+
-    geom_path(na.rm = TRUE, arrow = arrow1, size = 1) +  scale_color_distiller(palette = "RdYlGn",direction = 1)+
-    scale_x_continuous(limits = c(0,108), expand = c(0,0))+
-    scale_y_continuous(limits = c(0,93), expand = c(0,0))
-  ggsave(filename = fullname,p1)
-}
-
-for (i in 1:length(objects_21212_dir2$V1)) {
-  filename1 <- paste("plots/TrjMap/212x_dir2/21212",objects_21212_dir2$V1[i],sep = "_")
-  fullname <- paste(filename1, ".jpg",sep = "")
-  p1 <- Traj_Obj_212x_clipped2_dir2[(Traj_Obj_212x_clipped2_dir2$comp == "21212"&Traj_Obj_212x_clipped2_dir2$Obj == objects_21212_dir2$V1[i]),] %>% 
-    ggplot(aes(X,Y, col = V_mps_corrected, group = Obj))+
-    annotation_custom(rasterGrob(ima_212, width = unit(1,"npc"), 
-                                 height = unit(1,"npc")), 
-                      0, 108, 0, 74)+
-    geom_path(na.rm = TRUE, arrow = arrow1, size = 1) +  scale_color_distiller(palette = "RdYlGn",direction = 1)+
-    scale_x_continuous(limits = c(0,108), expand = c(0,0))+
-    scale_y_continuous(limits = c(0,93), expand = c(0,0))
-  ggsave(filename = fullname,p1)
-}
-
-for (i in 1:length(objects_21213_dir2$V1)) {
-  filename1 <- paste("plots/TrjMap/212x_dir2/21213",objects_21213_dir2$V1[i],sep = "_")
-  fullname <- paste(filename1, ".jpg",sep = "")
-  p1 <- Traj_Obj_212x_clipped2_dir2[(Traj_Obj_212x_clipped2_dir2$comp == "21213"&Traj_Obj_212x_clipped2_dir2$Obj == objects_21213_dir2$V1[i]),] %>% 
-    ggplot(aes(X,Y, col = V_mps_corrected, group = Obj))+
-    annotation_custom(rasterGrob(ima_212, width = unit(1,"npc"), 
-                                 height = unit(1,"npc")), 
-                      0, 108, 0, 74)+
-    geom_path(na.rm = TRUE, arrow = arrow1, size = 1) +  scale_color_distiller(palette = "RdYlGn",direction = 1)+
-    scale_x_continuous(limits = c(0,108), expand = c(0,0))+
-    scale_y_continuous(limits = c(0,93), expand = c(0,0))
-  ggsave(filename = fullname,p1)
-}
-
-for (i in 1:length(objects_21214_dir2$V1)) {
-  filename1 <- paste("plots/TrjMap/212x_dir2/21214",objects_21214_dir2$V1[i],sep = "_")
-  fullname <- paste(filename1, ".jpg",sep = "")
-  p1 <- Traj_Obj_212x_clipped2_dir2[(Traj_Obj_212x_clipped2_dir2$comp == "21214"&Traj_Obj_212x_clipped2_dir2$Obj == objects_21214_dir2$V1[i]),] %>% 
-    ggplot(aes(X,Y, col = V_mps_corrected, group = Obj))+
-    annotation_custom(rasterGrob(ima_212, width = unit(1,"npc"), 
-                                 height = unit(1,"npc")), 
-                      0, 108, 0, 74)+
-    geom_path(na.rm = TRUE, arrow = arrow1, size = 1) +  scale_color_distiller(palette = "RdYlGn",direction = 1)+
-    scale_x_continuous(limits = c(0,108), expand = c(0,0))+
-    scale_y_continuous(limits = c(0,93), expand = c(0,0))
-  ggsave(filename = fullname,p1)
-}
-
-for (i in 1:length(objects_21215_dir2$V1)) {
-  filename1 <- paste("plots/TrjMap/212x_dir2/21215",objects_21215_dir2$V1[i],sep = "_")
-  fullname <- paste(filename1, ".jpg",sep = "")
-  p1 <- Traj_Obj_212x_clipped2_dir2[(Traj_Obj_212x_clipped2_dir2$comp == "21215"&Traj_Obj_212x_clipped2_dir2$Obj == objects_21215_dir2$V1[i]),] %>% 
-    ggplot(aes(X,Y, col = V_mps_corrected, group = Obj))+
-    annotation_custom(rasterGrob(ima_212, width = unit(1,"npc"), 
-                                 height = unit(1,"npc")), 
-                      0, 108, 0, 74)+
-    geom_path(na.rm = TRUE, arrow = arrow1, size = 1) +  scale_color_distiller(palette = "RdYlGn",direction = 1)+
+    geom_path(na.rm = TRUE, arrow = arrow1, size = 1) +
     scale_x_continuous(limits = c(0,108), expand = c(0,0))+
     scale_y_continuous(limits = c(0,93), expand = c(0,0))
   ggsave(filename = fullname,p1)
